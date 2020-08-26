1fb4f883d77acc9fb09177151629c025b56ab158
molly mk <mmkover@gmail.com>
2020-08-26 14:47:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog 
 
-def name= (new_dog_name)
-  @dog_name = new_dog_name
-end 
+# def name= (new_dog_name)
+#   @dog_name = new_dog_name
+# end 
 
-def name
-  @dog_name 
-end
+# def name
+#   @dog_name 
+# end
 
 end 
